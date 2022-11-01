6e9121d5c0a2b96366acaeb7fefb7a96d882c0bb
Shantanu Soni <75296055+shantanusoni72@users.noreply.github.com>
2022-11-01 15:02:21 +0530

    Add files via upload

---
+++ precache-manifest.1cca63832b0e8f1a5d81a8211e4d0dd3.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8ceb5ff882507084d52557faf0c9ce",
+    "url": "/profile/index.html"
+  },
+  {
+    "revision": "2e7af4d9a590f659b984",
+    "url": "/profile/static/css/main.3e48aac0.chunk.css"
+  },
+  {
+    "revision": "e2c8516d2b5708187cba",
+    "url": "/profile/static/js/2.76c27ccb.chunk.js"
+  },
+  {
+    "revision": "32eaebd0b06f4bb60d5e9e5d0e7cd13e",
+    "url": "/profile/static/js/2.76c27ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7af4d9a590f659b984",
+    "url": "/profile/static/js/main.522316ea.chunk.js"
+  },
+  {
+    "revision": "93db51cde7503d9b538f",
+    "url": "/profile/static/js/runtime-main.1a388990.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/profile/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/profile/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/profile/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/profile/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/profile/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/profile/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/profile/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "bb2364632c8098d9b448ce5bda4fc26e",
+    "url": "/profile/static/media/MinecraftTen-VGORe.bb236463.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/profile/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "a6c1d39637949a370f8e93aab60a741f",
+    "url": "/profile/static/media/aws_logo.a6c1d396.png"
+  },
+  {
+    "revision": "7927bdc3aacc9c885db69227cc781c46",
+    "url": "/profile/static/media/contact.7927bdc3.png"
+  },
+  {
+    "revision": "79c3af4e26cace1f89ad5427225cc850",
+    "url": "/profile/static/media/education.79c3af4e.gif"
+  },
+  {
+    "revision": "118f7c9420a87176f9241630649aa68a",
+    "url": "/profile/static/media/experience.118f7c94.gif"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/profile/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/profile/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/profile/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/profile/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/profile/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/profile/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/profile/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/profile/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/profile/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/profile/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/profile/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/profile/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/profile/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/profile/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/profile/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "45814622cfdbd9666b39ca826efa50c1",
+    "url": "/profile/static/media/googlecloud_logo.45814622.png"
+  },
+  {
+    "revision": "499aa79a142268393a198dbedd55d3c1",
+    "url": "/profile/static/media/home.499aa79a.gif"
+  },
+  {
+    "revision": "d7f214fd4d34b50d6c67a157a55d0bef",
+    "url": "/profile/static/media/projects.d7f214fd.gif"
+  },
+  {
+    "revision": "23a3dafcea8c61072bc962348b16becb",
+    "url": "/profile/static/media/svvv.23a3dafc.png"
+  },
+  {
+    "revision": "13078d3724fd881a36204970293c49d1",
+    "url": "/profile/static/media/udemy_logo.13078d37.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0951a412a80d86de747ed99ed73bdf51.js"
+  "/profile/precache-manifest.1cca63832b0e8f1a5d81a8211e4d0dd3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/profile/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
